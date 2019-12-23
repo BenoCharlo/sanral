abef14a8a338b223da42f44ca28986e2d2226bee
BenoCharlo <dokodjokwassibenocharles@yahoo.fr>
2019-12-24 00:41:35 +0100

    create coord function

--- src/preprocessing.py
+++ src/preprocessing.py
@@ -68,3 +68,24 @@ def create_target(data, new_data):
     new_data.drop("datetime_segment", axis=1, inplace=True)
 
     return new_data
+
+
+def create_coord(data, new_data, coherent_id):
+
+    assert set(["road_segment_id", "longitude", "latitude"]).issubset(data.columns)
+    assert set(["datetime"]).issubset(new_data.columns)
+
+    latitudes = [
+        data.loc[data["road_segment_id"] == road_id]["latitude"].unique()[0]
+        for road_id in coherent_id
+    ]
+    longitudes = [
+        data.loc[data["road_segment_id"] == road_id]["longitude"].unique()[0]
+        for road_id in coherent_id
+    ]
+
+    new_data["latitude"] = latitudes * len(new_data["datetime"].unique())
+    new_data["longitude"] = longitudes * len(new_data["datetime"].unique())
+
+    return new_data
+
